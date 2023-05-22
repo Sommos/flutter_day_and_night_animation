af934966979f1080c8cc661f9bcd9fb6ba95b5a3
Sommos <Sommosgaming@gmail.com>
2023-05-22 11:02:44 +0100

    starting foundations

---
+++ README.md
@@ -0,0 +1,18 @@
+# Flutter Day And Night Animation 
+
+<p align="center">
+    ![Preview](/intro.gif)
+</p>
+
+A simple application that shows a login page with a switch to allow for a day and night mode
+
+# Contribution 
+
+If you'd like to contribute to `flutter_day_and_night_animation` please submit a pull-request on a feature branch.
+
+# Installing
+
+Clone the repo:
+
+    git clone https://github.com/Sommos/flutter_day_and_night_animation
+    cd flutter_day_and_night_animation
---
+++ lib/screens/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:animation/screens/login/login_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Day and night animation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
